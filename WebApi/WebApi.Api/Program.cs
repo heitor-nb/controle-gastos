532e94fd6690f0ee851b01d30ea3f5b277b74cdb
heitor <heitor.nb10@gmail.com>
2026-03-04 14:05:00 -0300

    add cors

--- WebApi/WebApi.Api/Program.cs
+++ WebApi/WebApi.Api/Program.cs
@@ -5,6 +5,17 @@ using WebApi.Infra.Persistance;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddControllers();
 
 builder.Services.AddPersistanceServices(builder.Configuration);
@@ -14,6 +25,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors("AllowFrontend");
 
 app.UseSwagger();
 app.UseSwaggerUI();
